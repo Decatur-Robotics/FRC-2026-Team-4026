36a5523828c8e96e0ff8f170fa11d65ad0a3d776
Charles Kennedy <82charkenn@csdecatur.net>
2026-01-11 17:21:33 -0500

    auto "fixed"

--- FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
@@ -92,8 +92,8 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  // public Command getAutonomousCommand() {
-  //   // An example command will be run in autonomous
-
-  // }
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+      return null;
+  }
 }
