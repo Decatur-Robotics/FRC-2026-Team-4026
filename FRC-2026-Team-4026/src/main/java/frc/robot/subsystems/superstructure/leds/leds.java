b96821a075d37d87ab939c532e7e4400281762c9
logan1102 <loganpark1102@gmail.com>
2026-01-16 17:34:40 -0500

    finished basic(boring) led system

--- FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/superstructure/leds/leds.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/superstructure/leds/leds.java
@@ -1,5 +1,109 @@
 package frc.robot.subsystems.superstructure.leds;
+import javax.sound.sampled.Port;
 
-public class leds {
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.constants.Ports;
+import frc.robot.util.TeamColor;
+public class leds implements Subsystem {
+
+    // an led strip
+    private AddressableLED led;
+    // how many leds on the strip
+    private int length;
+    // a buffer with data on the led strip states
+    private AddressableLEDBuffer buffer;
+    // the current desired color of the leds
+    private TeamColor currentColor;
+    // the color when the leds are off
+    private final TeamColor offColor;
+    //number of remaining led flashes
+    private int numFlashes;
+    //timer for flashing leds
+    private int periodsPassed;
+
+    private boolean on;
+
+    public leds(){
+
+        led = new AddressableLED(Ports.ADDRESSABLE_LED);
+
+        this.length = ledsConstants.LENGTH;
+        buffer = new AddressableLEDBuffer(length);
+
+        led.setLength(length);
+        led.setData(buffer);
+        led.start();
+
+
+        numFlashes = 0;
+        periodsPassed = 0;
+
+        on = true;
+        offColor = ledsConstants.OFF_COLOR;
+        currentColor = offColor;
+    }
+
+    public void updateData(){
+        led.setData(buffer);
+    }
+
+    public void setAllPixels(TeamColor color){
+        currentColor = color;
+
+        for (int i = 0; i < length; i++){
+            buffer.setRGB(i, color.r, color.g, color.b);
+        }
+        this.updateData();
+    }
+
+    public void flashAllPixels(TeamColor color, int numFlashes){
+        setAllPixels(color);
+        this.numFlashes = numFlashes;
+    }
     
+
+    @Override
+    public void periodic(){
+        periodsPassed ++;
+
+        if (periodsPassed == 5 && numFlashes >0){
+
+            if(on){
+
+                for (int i = 0; i < length;i++){
+                    buffer.setRGB(i, offColor.r, offColor.g, offColor.b);
+                }
+
+                this.updateData();
+                on = false;
+            }
+            else{
+
+                for(int i = 0; i< length;i++){
+                    buffer.setRGB(i, currentColor.r, currentColor.g, currentColor.b);
+                }
+
+                this.updateData();
+                on = true;
+                numFlashes --;
+            }
+        }
+
+
+        if (periodsPassed > 5) {
+			periodsPassed = 0;
+		}
+
+    }
+
+
+    public int getLength(){
+        return length;
+    }
+
+    public TeamColor getCurrentColor(){
+        return currentColor;
+    }
 }
--- FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/superstructure/leds/ledsConstants.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/superstructure/leds/ledsConstants.java
@@ -1,9 +1,8 @@
 package frc.robot.subsystems.superstructure.leds;
 
 import frc.robot.util.TeamColor;
-
 public class ledsConstants {
-    
+	public static final int LENGTH = 0;
 
     public static final TeamColor RED = new TeamColor(255,0,0);
     public static final TeamColor GREEN = new TeamColor(0, 255, 0);
@@ -13,4 +12,7 @@ public class ledsConstants {
 	public static final TeamColor MAGENTA = new TeamColor(255, 0, 255);
 	public static final TeamColor WHITE = new TeamColor(255, 255, 255);
 	public static final TeamColor BLACK = new TeamColor(0, 0, 0);
+
+	public static final TeamColor OFF_COLOR = new TeamColor(0,0,0);
+	
 }
