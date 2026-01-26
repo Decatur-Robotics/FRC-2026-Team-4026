9ec1b5dd7878297080871beb241614340bc3c3a4
Charles Kennedy <146139562+PPenguin1@users.noreply.github.com>
2026-01-26 15:42:05 -0500

    Revert "Shooter"

--- FRC-2026-Team-4026/src/main/java/frc/robot/shooter/ShooterConstants.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.shooter;
-
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-
-public class ShooterConstants {
-
-    public static final double FUEL_REST_VELOCITY = 0.0;
-
-}
