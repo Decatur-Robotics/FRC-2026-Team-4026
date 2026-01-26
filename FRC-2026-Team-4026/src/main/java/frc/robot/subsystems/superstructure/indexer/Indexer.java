9c30072b665ef611fb4a038c7ef7669d75c0f66e
Charles Kennedy <82charkenn@csdecatur.net>
2026-01-26 15:08:22 -0500

    fixed imports

--- FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/superstructure/indexer/Indexer.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/superstructure/indexer/Indexer.java
@@ -5,7 +5,6 @@ import org.littletonrobotics.junction.AutoLog;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.indexer.IndexerIOInputsAutoLogged;
 
 public class Indexer extends SubsystemBase{
     private double voltage;
