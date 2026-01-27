1015af29593c2dc5c7fbe34ffd9fdf023e5f36a4
le paul <97747093+seankid@users.noreply.github.com>
2026-01-27 17:23:07 -0500

    i made sim (its probably missing something)

--- FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/indexer/IndexerIOSim.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/indexer/IndexerIOSim.java
@@ -1,5 +1,25 @@
 package frc.robot.subsystems.indexer;
 
-public class IndexerIOSim {
-    //not sure what goes here yet.
+import edu.wpi.first.math.MatBuilder;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.numbers.*;
+
+public class IndexerIOSim implements IndexerIO {
+    //i know that something's probably missing here, so comment what it is if you know.
+    public final DCMotor gearbox;
+    private boolean closedLoop = false;
+    public IndexerIOSim() {
+       gearbox = DCMotor.getKrakenX44(2).withReduction(0);
+    }
+
+    @Override
+    public void updateInputs(IndexerIOInputs inputs) {
+       inputs.indexerData = new IndexerIOData(true, true, 0.0, 0.0, 0.0, 0.0);
+    }
+
+    public void runOpenLoop(double voltage){
+        closedLoop = false;
+    }
 }
