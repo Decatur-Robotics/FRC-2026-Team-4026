0927ad249514036a033cdbd2d63628c7bdfd635d
Charles Kennedy <82charkenn@csdecatur.net>
2026-01-29 17:57:17 -0500

    merge?

--- FRC-2026-Team-4026/src/main/java/frc/robot/util/PhoenixUtil.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/util/PhoenixUtil.java
@@ -147,6 +147,7 @@ public class PhoenixUtil {
      * <h4>Note:This function is skipped when running on a real robot, ensuring no impact on constants used on real
      * robot hardware.</h4>
      */
+    
     public static SwerveModuleConstants regulateModuleConstantForSimulation(
             SwerveModuleConstants<?, ?, ?> moduleConstants) {
         // Skip regulation if running on a real robot
