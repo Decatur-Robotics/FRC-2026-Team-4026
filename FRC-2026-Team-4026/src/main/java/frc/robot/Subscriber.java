58e61e03e905073be9e8d6c292d6a85f01823b8f
AllenGong12 <allenlgong@gmail.com>
2026-02-06 17:04:55 -0500

    comments

--- FRC-2026-Team-4026/src/main/java/frc/robot/Subscriber.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/Subscriber.java
@@ -5,15 +5,20 @@ import edu.wpi.first.networktables.DoubleTopic;
 public class Subscriber {
     private double value;
     final DoubleSubscriber dblSub;
+    //You need to make a subscriber object so that the subscriber lasts for the entire program
     public Subscriber(DoubleTopic dblTopic){
+        //subscribes to the networktable topic which connects it 
+        //and allows you to get the value of the subscriber/the network table topic.
         dblSub = dblTopic.subscribe(0);
 
 
     }
+    //This updates the value of the networktable topic
     public void periodic(){
         value = dblSub.get();
 
     }
+
     public double getValue(){
         return value;
     }
