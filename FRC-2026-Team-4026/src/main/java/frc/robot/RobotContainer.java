0428262f98f8798432efea71d6ca0bb4d1701fdb
Charles Kennedy <82charkenn@csdecatur.net>
2026-03-14 09:57:19 -0400

    control

--- FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
@@ -190,15 +190,15 @@ public class RobotContainer {
          drive.setDefaultCommand(
               DriveCommands.joystickDrive(
                 drive,
-                ()-> joystick.getY(),
-                ()-> joystick.getX(),
+                ()-> -joystick.getY(),
+                ()-> -joystick.getX(),
                 ()-> -joystick.getTwist()
             ));
 
           // y.whileTrue(drive.runOnce(() -> drive.setPose(new Pose2d(3.5, 6, new Rotation2d(0, 0)))));
           // a.whileTrue(drive.runOnce(() -> drive.setPose(new Pose2d(drive.getPose().getX(), drive.getPose().getY(), new Rotation2d(0, 0)))));
           // x.whileTrue(drive.setRotationXCommand());
-          b.whileTrue(drive.autoAlignToHub());
+          bumperRight.whileTrue(drive.autoAlignToHub());
           bumperLeft.whileTrue(drive.alignHubPathpl());
           triggerLeft.whileTrue(DriveCommands.joystickDrive(
                 drive,
--- FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/drive/Drive.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -521,7 +521,7 @@ public void driveToPose(Supplier<ChassisSpeeds> targetSpeeds, Supplier<Pose2d> t
     
 }
 
-private ProfiledPIDController angleController = new ProfiledPIDController(10.0, 0.0, 0.0, new TrapezoidProfile.Constraints(2, 2));
+private ProfiledPIDController angleController = new ProfiledPIDController(2.0, 0.0, 0.0, new TrapezoidProfile.Constraints(3, 4));
 public void autoAlign(Supplier<Rotation2d> targetRotation){
     angleController.enableContinuousInput(-Math.PI, Math.PI);
 
