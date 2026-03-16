5be5ee251057b31530f5f2aa5c2efc1d3678904f
Logan Park <loganpark1102@gmail.com>
2026-03-15 23:06:31 -0400

    changed oscillating intake to run off time not pose(and fixed spelling error)

--- FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/superstructure/intake/Intake.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/superstructure/intake/Intake.java
@@ -9,7 +9,11 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Timer;
+
 import static edu.wpi.first.units.Units.*;
+
+
 
 public class Intake extends SubsystemBase{
 
@@ -18,12 +22,13 @@ public class Intake extends SubsystemBase{
     private final SysIdRoutine sysIdRoutine = new SysIdRoutine(new SysIdRoutine.Config(Volts.of(0.4).per(Second), Volts.of(1.5),Seconds.of(10), (state) -> SignalLogger.writeString("state", state.toString())),
     new SysIdRoutine.Mechanism((volts) -> io.setDeployVoltage(volts.in(Volts)),null, this));
     private boolean osillatingIntakeGoingUp;
-
+    private Timer oscillatingTimer;
 
 
     public Intake(IntakeIO io){
         this.io = io;
         osillatingIntakeGoingUp = false;
+        oscillatingTimer = new Timer();
     }
 
     @Override
@@ -37,17 +42,19 @@ public class Intake extends SubsystemBase{
         Logger.recordOutput("Intake Current", getIntakeCurrent());
     }
 
-    public void osillatingIntake(){
+    public void oscillatingIntake(){
         if (osillatingIntakeGoingUp){
             io.setDeployPosition(IntakeConstants.HALFWAY_INTAKE_POSITION);
             if(MathUtil.applyDeadband(getDeployPosition(),0.8) == IntakeConstants.HALFWAY_INTAKE_POSITION){
                 osillatingIntakeGoingUp = false;
+                oscillatingTimer.restart();
             }
         }
         if (!osillatingIntakeGoingUp){
             io.setDeployPosition(IntakeConstants.DEPLOY_INTAKE_POSITION);
-            if(MathUtil.applyDeadband(getDeployPosition(),0.8) == IntakeConstants.DEPLOY_INTAKE_POSITION){
+            if(oscillatingTimer.get() > 1.0){
                 osillatingIntakeGoingUp = true;
+                
             }
         }
 
@@ -78,8 +85,8 @@ public class Intake extends SubsystemBase{
     }
 
 
-    public Command osillatingIntakeCommand(){
-        return Commands.runOnce(()->osillatingIntake());
+    public Command oscillatingIntakeCommand(){
+        return Commands.runOnce(()->oscillatingIntake());
     }
     public Command slamIntakeCommand(double position){
         return Commands.runOnce(() -> io.setDeployVoltage(position));
