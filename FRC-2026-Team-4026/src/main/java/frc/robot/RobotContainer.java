af04871e3d4c6eb418a3b3ead83e923aea0c8db7
Charles Kennedy <82charkenn@csdecatur.net>
2026-03-19 15:04:21 -0400

    logs will now go to usb drive and added indexing customization for shooting

--- FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
@@ -236,13 +236,15 @@ public class RobotContainer {
         JoystickButton triggerRight = new JoystickButton(joystick, LogitechControllerButtons.triggerRight);
 
         triggerLeft.whileTrue(superstructure.shootCommand(() -> 42.0)).onFalse(superstructure.storeCommand());
+        triggerLeft.and(right).whileTrue(superstructure.oscillateShootCommand(() -> 42.0)).onFalse(superstructure.storeCommand());
+        triggerLeft.and(bumperRight).whileTrue(superstructure.pushShootCommand(() -> 42.0, () -> joystick.getX()));
         triggerRight.whileTrue(superstructure.shootCommand()).onFalse(superstructure.storeCommand());
-        // bumperLeft.whileTrue(superstructure.passCommand()).onFalse(superstructure.storeCommand());
+        triggerRight.and(right).whileTrue(superstructure.oscillateShootCommand()).onFalse(superstructure.storeCommand());
+        triggerRight.and(bumperRight).whileTrue(superstructure.pushShootCommand(() -> joystick.getX()));
         a.whileTrue(superstructure.intakeCommand()).onFalse(superstructure.storeCommand());
         y.whileTrue(intake.deployIntakeCommand(IntakeConstants.STORED_INTAKE_POSITION));
        b.whileTrue(intake.deployIntakeCommand(IntakeConstants.DEPLOY_INTAKE_POSITION));
         x.whileTrue(superstructure.dumpCommand()).onFalse(superstructure.storeCommand());
-      left.whileTrue(intake.deployIntakeCommand(7));
        right.whileTrue(intake.oscillateIntakeCommand());
        
   }
