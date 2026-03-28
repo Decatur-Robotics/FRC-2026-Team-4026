a77dee0535e86dec2e9eee492a176883dc9e8130
Charles Kennedy <82charkenn@csdecatur.net>
2026-03-27 20:07:17 -0400

    p .3

--- FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
@@ -267,7 +267,7 @@ private enum AutoSide{
 
         // bumperLeft.whileTrue(superstructure.passCommand()).onFalse(superstructure.storeCommand());
         // triggerLeft.whileTrue(superstructure.shootCommand(() -> 44.0)).onFalse(superstructure.storeCommand());
-        triggerLeft.whileTrue(shooter.setVelocityCommand(5)).onFalse(shooter.setVelocityCommand(0));
+        triggerLeft.whileTrue(shooter.setVelocityCommand(60)).onFalse(shooter.setVelocityCommand(0));
         y.whileTrue(shooter.sysIdDynamic(Direction.kForward));
         b.whileTrue(shooter.sysIdQuasistatic(Direction.kForward));
         
