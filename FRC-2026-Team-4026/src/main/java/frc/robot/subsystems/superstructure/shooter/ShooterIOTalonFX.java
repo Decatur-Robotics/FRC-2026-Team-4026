7fb9b4da9d60e084eb3a44acd5f7d03531441f57
Charles Kennedy <82charkenn@csdecatur.net>
2026-03-30 19:23:53 -0400

    correct logging for shooter

--- FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/superstructure/shooter/ShooterIOTalonFX.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/superstructure/shooter/ShooterIOTalonFX.java
@@ -82,12 +82,12 @@ public void updateInputs (ShooterIOInputs inputs){
         motor.isConnected(),
         followerMotor.isConnected(),
         motor.getVelocity().getValueAsDouble(),
-        followerMotor.getVelocity().getValueAsDouble(),
         motor.getMotorVoltage().getValueAsDouble(),
-        followerMotor.getMotorVoltage().getValueAsDouble(),
         motor.getSupplyCurrent().getValueAsDouble(),
-        followerMotor.getSupplyCurrent().getValueAsDouble(),
         motor.getDeviceTemp().getValueAsDouble(),
+        followerMotor.getVelocity().getValueAsDouble(),
+        followerMotor.getMotorVoltage().getValueAsDouble(),
+        followerMotor.getSupplyCurrent().getValueAsDouble(),
         followerMotor.getDeviceTemp().getValueAsDouble() );
 }
 
