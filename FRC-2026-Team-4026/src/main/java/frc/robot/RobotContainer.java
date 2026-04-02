aea3caed917e9933ad3f1753e03d98485987445d
Charles Kennedy <82charkenn@csdecatur.net>
2026-04-02 16:42:00 -0400

    pid noise reduction

--- FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
@@ -295,13 +295,13 @@ private enum AutoSide{
 
 
 
-        triggerLeft.whileTrue(Commands.parallel(shooter.setVelocityWithSlider(), indexer.setVoltageCommand(10))).onFalse(Commands.parallel(shooter.setVoltageCommand(0), indexer.setVoltageCommand(0)));
-
+        // triggerLeft.whileTrue(Commands.parallel(shooter.setVelocityWithSlider(), indexer.setVoltageCommand(10))).onFalse(Commands.parallel(shooter.setVoltageCommand(0), indexer.setVoltageCommand(0)));
+        triggerLeft.whileTrue(shooter.setVoltageCommand(6)).onFalse(shooter.setVoltageCommand(0));
     
         
         triggerLeft.and(right).whileTrue(superstructure.oscillateShootCommand(() -> 44.0)).onFalse(superstructure.storeCommand());
         triggerLeft.and(bumperRight).whileTrue(superstructure.pushShootCommand(() -> 44.0, () -> joystick.getY()));
-        triggerRight.whileTrue(drive.getShootOnMoveBoolean()? superstructure.shootOnMoveCommand():superstructure.shootCommand(()->47.84)).onFalse(superstructure.stopShooting());
+        triggerRight.whileTrue(drive.getShootOnMoveBoolean()? superstructure.shootOnMoveCommand():superstructure.shootCommand(()->50.0)).onFalse(superstructure.stopShooting());
         triggerRight.and(right).whileTrue(superstructure.oscillateShootCommand()).onFalse(superstructure.storeCommand());
         triggerRight.and(bumperRight).whileTrue(superstructure.pushShootCommand(() -> joystick.getY()));
         a.whileTrue(superstructure.intakeCommand()).onFalse(superstructure.storeCommand());
--- FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/superstructure/shooter/ShooterConstants.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/superstructure/shooter/ShooterConstants.java
@@ -14,9 +14,9 @@ import edu.wpi.first.units.measure.Distance;
 public class ShooterConstants {
 
     public static final double FUEL_REST_VELOCITY = 0.0;
-    public static final double kP = 0.5;
+    public static final double kP = 0.35;
     public static final double kI = 0.0;
-    public static final double kD = 0.013;
+    public static final double kD = 0.001;
     public static final double kS = 0.15;
     public static final double kV = 0.12;
     public static final double kA = 10.0;
