f6f2c32b5629262e92d8e4f41ae0aa899b35ba16
Charles Kennedy <82charkenn@csdecatur.net>
2026-04-06 18:13:53 -0400

    tweaked to columbus auto

--- FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
@@ -399,33 +399,12 @@ public class RobotContainer {
     // Command auto = Commands.sequence(new PathPlannerAuto("Center Test"), new
     // PathPlannerAuto("Center Test 2"));
     // return auto;
-    // PathPlannerAuto auto = new PathPlannerAuto("Center Rush Right");
-    // auto.activePath("Center Rush Right
-    // Intake").whileTrue(superstructure.intakeCommand()).onFalse(superstructure.storeCommand());
-    // auto.activePath("Center Rush Right
-    // shoot").onFalse(Commands.parallel(superstructure.oscillateShootCommand()));
-    // return auto;
+    PathPlannerAuto auto = new PathPlannerAuto("Center Rush Right");
+    auto.activePath("Center Rush Right Intake").whileTrue(superstructure.intakeCommand()).onFalse(superstructure.storeCommand());
+    auto.activePath("Center Rush Right shoot").onFalse(Commands.parallel(superstructure.oscillateShootCommand()));
+    return auto;
 
-    PathPlannerAuto auto1 = new PathPlannerAuto("Center Rush Right");
-    auto1
-        .activePath("Center Rush Right Intake").whileTrue(Commands
-            .parallel(intake.deployIntakeCommand(IntakeConstants.DEPLOY_INTAKE_POSITION), intake.runIntakeCommand(-12)))
-        .onFalse(intake.runIntakeCommand(0));
-
-    PathPlannerAuto auto2 = new PathPlannerAuto("Center Right 2 Test");
-    auto2.activePath("Center Rush Right 2").onTrue(Commands.parallel(shooter.setVoltageCommand(0),
-        indexer.setVoltageCommand(0), intake.deployIntakeCommand(IntakeConstants.DEPLOY_INTAKE_POSITION)));
-    auto2
-        .activePath("Center Rush 2 1002").whileTrue(Commands
-            .parallel(intake.deployIntakeCommand(IntakeConstants.DEPLOY_INTAKE_POSITION), intake.runIntakeCommand(-12)))
-        .onFalse(intake.runIntakeCommand(0));
-    // PathPlannerAuto auto3 = new PathPlannerAuto("Center Rush Right 2");
-
-    // auto1.activePath("Center Rush Right
-    // shoot").onFalse(Commands.sequence(superstructure.oscillateShootCommand(),
-    // Commands.waitTime(Seconds.of(3))));
-    return Commands.sequence(auto1,
-        Commands.parallel(shooter.shootAimCommand(), indexer.setVoltageCommand(10), intake.oscillateIntakeCommand()).withTimeout(Seconds.of(3)), auto2, superstructure.oscillateShootCommand());
+  
 
   }
 
