c9deaccdadc3d4cd3aff6c61a3888123581c4c9c
Charles Kennedy <82charkenn@csdecatur.net>
2026-04-06 09:49:24 -0400

    sequencential commands were never the problem, the 2nd auto was, and now it is fixed

--- FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
@@ -413,19 +413,12 @@ public class RobotContainer {
         .onFalse(intake.runIntakeCommand(0));
 
     PathPlannerAuto auto2 = new PathPlannerAuto("Center Right 2 Test");
-    auto2.activePath("Center Rush Right 2").onTrue(Commands.parallel(shooter.setVoltageCommand(0),
-        indexer.setVoltageCommand(0), intake.deployIntakeCommand(IntakeConstants.DEPLOY_INTAKE_POSITION)));
     auto2
         .activePath("Center Rush 2 1002").whileTrue(Commands
             .parallel(intake.deployIntakeCommand(IntakeConstants.DEPLOY_INTAKE_POSITION), intake.runIntakeCommand(-12)))
         .onFalse(intake.runIntakeCommand(0));
-    // PathPlannerAuto auto3 = new PathPlannerAuto("Center Rush Right 2");
-
-    // auto1.activePath("Center Rush Right
-    // shoot").onFalse(Commands.sequence(superstructure.oscillateShootCommand(),
-    // Commands.waitTime(Seconds.of(3))));
     return Commands.sequence(auto1,
-        Commands.parallel(shooter.shootAimCommand(), indexer.setVoltageCommand(10), intake.oscillateIntakeCommand()).withTimeout(Seconds.of(3)), auto2, superstructure.oscillateShootCommand());
+        superstructure.oscillateShootCommand().withTimeout(Seconds.of(3)), auto2, superstructure.oscillateShootCommand());
 
   }
 
