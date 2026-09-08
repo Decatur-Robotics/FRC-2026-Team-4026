651663df8d06d4b0e166f4fe08208d473a01ce93
le paul <97747093+seankid@users.noreply.github.com>
2026-09-08 16:17:06 -0400

    added kick motor jam detection code

--- FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/superstructure/indexer/IndexerIOTalonFX.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/subsystems/superstructure/indexer/IndexerIOTalonFX.java
@@ -103,7 +103,7 @@ public class IndexerIOTalonFX implements IndexerIO{
 
         voltageRequest = new VoltageOut(voltage*-1);
         mecanumMotor.setControl(voltageRequest);
-        if(mecanumMotor.getSupplyCurrent().getValueAsDouble() > 65 && mecanumMotor.getVelocity().getValueAsDouble() == 0){
+        if((mecanumMotor.getSupplyCurrent().getValueAsDouble() > 65 && mecanumMotor.getVelocity().getValueAsDouble() <= 0.1) || (kickMotor.getSupplyCurrent().getValueAsDouble() > 65 && kickMotor.getVelocity().getValueAsDouble() <= 0.1)){
             setVoltage(-6);
             Timer.delay(0.7);
             setVoltage(6);
