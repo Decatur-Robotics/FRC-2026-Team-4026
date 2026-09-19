981960e5ddb620a43e5728aeee6dda60862df12c
Charles Kennedy <82charkenn@csdecatur.net>
2026-09-19 01:42:12 -0400

    got rid of not working binding

--- FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
+++ FRC-2026-Team-4026/src/main/java/frc/robot/RobotContainer.java
@@ -347,7 +347,7 @@ public class RobotContainer {
                 x.whileTrue(superstructure.dumpCommand()).onFalse(superstructure.storeCommand());
                 right.whileTrue(intake.oscillateIntakeCommand());
                 up.whileTrue(indexer.setVoltageCommand(-10)).onFalse(indexer.setVoltageCommand(0));
-                down.whileTrue(intake.setSlowPositionCommand());
+                // down.whileTrue(intake.setSlowPositionCommand());
 
                 bumperLeft.whileTrue(superstructure.passCommand()).onFalse(superstructure.storeCommand());
                 bumperLeft.and(a).whileTrue(superstructure.passIntakeCommand());
